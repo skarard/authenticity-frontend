f1fcb005afaaa5d2e6b779c89da400d6065757c9
skarard <skarard@gmail.com>
2023-02-01 00:33:43 +0000

    Added basic hashing function

--- components/ImageProcessor.tsx
+++ components/ImageProcessor.tsx
@@ -1,10 +1,62 @@
+import { Button } from "@mui/material";
+import { ethers } from "ethers";
 import React, { useState } from "react";
 import FileUpload from "./FileUpload";
+
+function readFileDataAsArrayBuffer(file: File): Promise<string | ArrayBuffer> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      resolve(event.target.result);
+    };
+
+    reader.onerror = (err) => {
+      reject(err);
+    };
+
+    reader.readAsArrayBuffer(file);
+  });
+}
 
 const ImageProcessor = () => {
   const [files, setFiles] = useState<File[] | null>(null);
 
-  return <FileUpload files={files} setFiles={setFiles} />;
+  const processImages = async () => {
+    const hashedFiles = await Promise.all(
+      files.map(async (file) => {
+        const hashedFile = file as File & { hash: string };
+        const arrayBuffer = await readFileDataAsArrayBuffer(file);
+        if (typeof arrayBuffer === "string") return;
+
+        const bytes =
+          "0x" +
+          [...new Uint8Array(arrayBuffer)]
+            .map((x) => x.toString(16).padStart(2, "0"))
+            .join("");
+
+        hashedFile.hash = ethers.utils.solidityKeccak256(["bytes"], [bytes]);
+        return hashedFile;
+      })
+    );
+
+    console.log(
+      Object.fromEntries(hashedFiles.map((file) => [file.name, file.hash]))
+    );
+  };
+  return (
+    <>
+      <Button
+        className="w-full"
+        variant="contained"
+        disabled={!(files && files.length > 0)}
+        onClick={processImages}
+      >
+        Verify Images
+      </Button>
+      <FileUpload files={files} setFiles={setFiles} />
+    </>
+  );
 };
 
 export default ImageProcessor;
