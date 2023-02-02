8b6cd55ec6360092b916c2e268bfcbf109686b2b
skarard <skarard@gmail.com>
2023-02-02 21:57:01 +0000

    Add env cars to managing the active chain and the contract to connect

--- config/index.ts
+++ config/index.ts
@@ -6,6 +6,16 @@ const config = {
       process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID,
       "NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID"
     ),
+    ActiveChain: parseEnvVar(
+      process.env.NEXT_PUBLIC_ACTIVE_CHAIN,
+      "NEXT_PUBLIC_ACTIVE_CHAIN"
+    ),
+  },
+  Contracts: {
+    Authenticity: parseEnvVar(
+      process.env.NEXT_PUBLIC_AUTHENTICITY_CONTRACT,
+      "NEXT_PUBLIC_AUTHENTICITY_CONTRACT"
+    ),
   },
 };
 
