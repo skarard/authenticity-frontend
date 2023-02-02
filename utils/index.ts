a9021175b4d992bbad6b95a332516d38d21360c9
skarard <skarard@gmail.com>
2023-02-02 21:58:01 +0000

    Add a utility for fetching the active chain in wagmi format

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,17 @@
+import { Chain, localhost, polygon, polygonMumbai } from "wagmi/chains";
+
 export const parseEnvVar = (value: string, name: string) => {
   if (!value) throw Error(`Environment variable ${name} is not set.`);
   return value;
+};
+
+const chains: Record<string, Chain> = {
+  localhost: localhost,
+  polygon: polygon,
+  polygonMumbai: polygonMumbai,
+};
+
+export const getActiveChain = (chain: string) => {
+  if (!(chain in chains)) throw Error(`Chain ${chain} not supported`);
+  return chains[chain];
 };
