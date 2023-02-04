9f069f8e3359de84ac0b27da928e5e1127efb7c6
skarard <skarard@gmail.com>
2023-02-04 08:50:06 +0000

    two-column layout on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,24 @@ import { useAccount } from "wagmi";
 import ImageProcessor from "components/ImageProcessor";
 import ClientOnly from "components/ClientOnly";
 import { Box } from "@mui/system";
+import Skeleton from "@mui/material/Skeleton";
 
 const IndexPage = () => {
   const { isConnected } = useAccount();
 
   return (
-    <Box className="h-full flex flex-row gap-4 items-center">
-      <FlexSpacer className="max-h-48 min-h-[100px]" />
-      <h1 className="text-8xl my-6">Authenticity</h1>
-      <ClientOnly>
-        <ImageProcessor />
-      </ClientOnly>
+    <Box className="h-full flex flex-col">
+      <FlexSpacer className="w-full max-h-48 min-h-[100px]" />
+      <Box className="h-full flex flex-row gap-40">
+        <Box className="h-full flex flex-col basis-1/2 items-center justify-center">
+          <ClientOnly>
+            <ImageProcessor />
+          </ClientOnly>
+        </Box>
+        <Box className="h-full flex flex-col basis-1/2 items-center justify-center">
+          <Skeleton variant="rectangular" width="100%" height="75%" />
+        </Box>
+      </Box>
       <FlexSpacer className="min-h-[100px]" />
     </Box>
   );
