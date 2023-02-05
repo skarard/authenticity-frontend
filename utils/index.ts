346e5c8dd92288f4cf2148f31e3e74b8cae89061
skarard <skarard@gmail.com>
2023-02-05 09:03:08 +0000

    Adding blog utils for fetching data from markdown

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,7 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
 import { Chain, localhost, polygon, polygonMumbai } from "wagmi/chains";
 
 export const parseEnvVar = (value: string, name: string) => {
@@ -14,4 +18,47 @@ const chains: Record<string, Chain> = {
 export const getActiveChain = (chain: string) => {
   if (!(chain in chains)) throw Error(`Chain ${chain} not supported`);
   return chains[chain];
+};
+
+export const getPath = (folder: string) => {
+  return path.join(process.cwd(), `/${folder}`); // Get full path
+};
+
+export const getFileContent = (filename: string, folder: string) => {
+  const POSTS_PATH = getPath(folder);
+  return fs.readFileSync(path.join(POSTS_PATH, filename), "utf8");
+};
+
+export const getAllPosts = (folder: string) => {
+  const POSTS_PATH = getPath(folder);
+  return fs
+    .readdirSync(POSTS_PATH) // get files in directory
+    .filter((path) => /\\.md?$/.test(path)) // only .md files
+    .map((fileName) => {
+      // map over each file
+      const source = getFileContent(fileName, folder); // retrieve the file contents
+      const slug = fileName.replace(/\\.md?$/, ""); // get the slug from the filename
+      const { data } = matter(source); // extract frontmatter
+      return {
+        frontmatter: data,
+        slug: slug,
+      };
+    });
+};
+
+export const getAllPublished = (folder: string) => {
+  const posts = getAllPosts(folder);
+  const published = posts.filter((post) => {
+    return post.frontmatter.isPublished === true;
+  });
+  return published;
+};
+
+export const getSinglePost = (slug: string, folder: string) => {
+  const source = getFileContent(`${slug}.md`, folder);
+  const { data: frontmatter, content } = matter(source);
+  return {
+    frontmatter,
+    content,
+  };
 };
