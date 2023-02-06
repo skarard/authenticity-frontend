f5e5bf4376cdfb5571f19eb6ef12121093a3c258
skarard <skarard@gmail.com>
2023-02-06 00:26:09 +0000

    Improve layout of blog post page

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,14 +1,46 @@
+import { Box, Skeleton } from "@mui/material";
+import Chip from "@mui/material/Chip";
+import Divider from "@mui/material/Divider";
+import Image from "next/image";
 import ReactMarkdown from "react-markdown";
-import { getAllPosts, getAllPublished, getSinglePost } from "utils/md";
+import { BlogPost, getAllPublished, getSinglePost } from "utils/md";
 
-const Post = ({ content, frontmatter }) => {
+const Post = ({ content, frontmatter }: BlogPost & { content }) => {
   return (
-    <div>
-      <p>{frontmatter.tags.join(", ")}</p>
-      <h2>{frontmatter.title}</h2>
-      <span>{frontmatter.publishedDate}</span>
+    <>
+      <h1>{frontmatter.title}</h1>
+      <Box className="flex flex-row gap-2 text-base">
+        <Box>
+          by: <span className="italic">{frontmatter.author}</span>
+        </Box>
+        <Divider orientation="vertical" />
+        <Box>
+          <span className="italic">
+            {new Date(Date.parse(frontmatter.publishedDate)).toLocaleString(
+              "default",
+              { day: "numeric", month: "long", year: "numeric" }
+            )}
+          </span>
+        </Box>
+      </Box>
+      <Box className="my-8">
+        {frontmatter.image ? (
+          <Image src={frontmatter.image} fill alt={frontmatter.imageAlt} />
+        ) : (
+          <Divider />
+        )}
+      </Box>
       <ReactMarkdown>{content}</ReactMarkdown>
-    </div>
+      <Divider>
+        {frontmatter.tags.map((tag) => (
+          <Chip
+            className="mx-2 text-yellow-50"
+            label={tag}
+            variant="outlined"
+          />
+        ))}
+      </Divider>
+    </>
   );
 };
 
