8eef9a92feeedd80bf7a9cecaf7440de30181db9
skarard <skarard@gmail.com>
2023-02-06 00:26:52 +0000

    Add a featured article and improve layout of published post list

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,30 +1,59 @@
-import { Box } from "@mui/material";
+import { Box, Chip, Divider } from "@mui/material";
+import Skeleton from "@mui/material/Skeleton";
+import Image from "next/image";
 import Link from "next/link";
-import { getAllPosts, getAllPublished } from "utils/md";
+import { BlogPost, getAllPublished, getFeatured } from "utils/md";
 
-interface BlogPost {
-  frontmatter: { [key: string]: any };
-  slug: string;
-}
-
-const BlogPage = ({ posts }: { posts: BlogPost[] }) => {
+const BlogPage = ({
+  posts,
+  featured,
+}: {
+  posts: BlogPost[];
+  featured: BlogPost;
+}) => {
   return (
     <Box>
-      Blogs
+      <Link href={`blog/${featured.slug}`}>
+        <Box>
+          <h1>{featured.frontmatter.title}</h1>
+          <Box className="bg-slate-700/20 w-full h-[480px] relative">
+            <Image
+              src={featured.frontmatter.image}
+              alt={featured.frontmatter.imageAlt}
+              height={480}
+            />
+            <Box className="absolute bottom-0 mb-4 px-6 py-2 bg-slate-700/75">
+              {featured.frontmatter.description}
+            </Box>
+          </Box>
+        </Box>
+      </Link>
       {posts.map((post) => (
-        <article key={post.slug}>
-          <p>{post.frontmatter.tags.join(" ")}</p>
-          <Link href={`blog/${post.slug}`}>{post.frontmatter.title}</Link>
-          <p>{post.frontmatter.description}</p>
-        </article>
+        <Link className="group" href={`blog/${post.slug}`}>
+          <article className="group-hover:bg-sky-100/5 p-10" key={post.slug}>
+            <h2>{post.frontmatter.title}</h2>
+            <Box className="mb-4">{post.frontmatter.description}</Box>
+            <Divider>
+              {post.frontmatter.tags.map((tag) => (
+                <Chip
+                  className="mx-2 text-yellow-50"
+                  label={tag}
+                  variant="outlined"
+                  size="small"
+                />
+              ))}
+            </Divider>
+          </article>
+        </Link>
       ))}
     </Box>
   );
 };
 export const getStaticProps = async () => {
   const posts = getAllPublished("content");
+  const featured = getFeatured("content");
   return {
-    props: { posts },
+    props: { posts, featured },
   };
 };
 export default BlogPage;
