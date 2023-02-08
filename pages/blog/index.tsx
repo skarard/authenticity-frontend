7240c2f5a5eae1148e590c8fe1ac2c2812fc6c13
skarard <skarard@gmail.com>
2023-02-08 22:37:02 +0000

    Improved formatting on blog index

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -12,9 +12,9 @@ const BlogPage = ({
   featured: BlogPost;
 }) => {
   return (
-    <Box>
+    <>
       <Link href={`blog/${featured.slug}`}>
-        <Box>
+        <Box className="mb-16">
           <h1>{featured.frontmatter.title}</h1>
           <Box className="bg-slate-700/20 w-full h-[480px] relative">
             <Image
@@ -22,31 +22,43 @@ const BlogPage = ({
               alt={featured.frontmatter.imageAlt}
               height={480}
             />
-            <Box className="absolute bottom-0 mb-4 px-6 py-2 bg-slate-700/75">
+            <Box className="absolute bottom-0 px-6 py-4 bg-slate-700/75">
               {featured.frontmatter.description}
             </Box>
           </Box>
         </Box>
       </Link>
-      {posts.map((post) => (
-        <Link className="group" href={`blog/${post.slug}`}>
-          <article className="group-hover:bg-sky-100/5 p-10" key={post.slug}>
-            <h2>{post.frontmatter.title}</h2>
-            <Box className="mb-4">{post.frontmatter.description}</Box>
-            <Divider>
-              {post.frontmatter.tags.map((tag) => (
-                <Chip
-                  className="mx-2 text-yellow-50"
-                  label={tag}
-                  variant="outlined"
-                  size="small"
-                />
-              ))}
-            </Divider>
-          </article>
-        </Link>
-      ))}
-    </Box>
+      <Box className="flex flex-row gap-6">
+        <Box className="basis-3/4">
+          {posts.map((post) => (
+            <Link className="group" href={`blog/${post.slug}`}>
+              <article
+                className="group-hover:bg-sky-100/5 p-10"
+                key={post.slug}
+              >
+                <h2>{post.frontmatter.title}</h2>
+                <Box className="mb-4">{post.frontmatter.description}</Box>
+                <Divider>
+                  {post.frontmatter.tags.map((tag) => (
+                    <Chip
+                      className="mx-2 text-yellow-50"
+                      label={tag}
+                      variant="outlined"
+                      size="small"
+                    />
+                  ))}
+                </Divider>
+              </article>
+            </Link>
+          ))}
+        </Box>
+        <Box className="flex flex-col basis-1/4 gap-6">
+          <Skeleton variant="rounded" width="100%" height="200px" />
+          <Skeleton variant="rounded" width="100%" />
+          <Skeleton variant="rounded" width="100%" />
+        </Box>
+      </Box>
+    </>
   );
 };
 export const getStaticProps = async () => {
